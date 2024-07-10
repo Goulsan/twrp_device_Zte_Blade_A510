e6ccd686e5793a96bbb258b883d8f08a5606a660
Goulsan <133015958+Goulsan@users.noreply.github.com>
2024-07-10 19:55:28 +0300

    Add files via upload

--- omni_P635A50.mk
+++ omni_P635A50.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := p1m
+PRODUCT_RELEASE_NAME := P635A50
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
@@ -11,8 +11,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := p1m
-PRODUCT_NAME := omni_p1m
-PRODUCT_BRAND := lenovo
-PRODUCT_MODEL := Lenovo p1m
-PRODUCT_MANUFACTURER := lenovo
+PRODUCT_DEVICE := P635A50
+PRODUCT_NAME := omni_P635A50
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := ZTE Blade A510
+PRODUCT_MANUFACTURER := zte
