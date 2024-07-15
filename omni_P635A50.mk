5957fe4d0b053719e08e1639d9fbac127fcf88a9
Goulsan <133015958+Goulsan@users.noreply.github.com>
2024-07-15 19:00:32 +0300

    Update omni_P635A50.mk

--- omni_P635A50.mk
+++ omni_P635A50.mk
@@ -5,14 +5,14 @@ PRODUCT_RELEASE_NAME := P635A50
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := P635A50
-PRODUCT_NAME := omni_P635A50
+PRODUCT_NAME := pb_P635A50
 PRODUCT_BRAND := zte
 PRODUCT_MODEL := ZTE Blade A510
 PRODUCT_MANUFACTURER := zte
