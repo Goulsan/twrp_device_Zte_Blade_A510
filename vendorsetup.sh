3ee71590ca176c55bcabecad2c192b4a25789a59
Goulsan <133015958+Goulsan@users.noreply.github.com>
2024-07-15 19:02:48 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_P635A50-user
-add_lunch_combo omni_P635A50-userdebug
-add_lunch_combo omni_P635A50-eng
+add_lunch_combo pb_P635A50-user
+add_lunch_combo pb_P635A50-userdebug
+add_lunch_combo pb_P635A50-eng
